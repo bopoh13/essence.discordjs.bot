792085581696e4c36298a3dbf1104b721a355af8
h8nor <bopoh13@yandex.com>
2022-10-05 23:55:24 +0300

    Check and install dependencies from `package.json`

--- start_bot.bat
+++ start_bot.bat
@@ -1,8 +1,13 @@
-TITLE Discord Bot - %~0
+@ECHO off
 
-node --version
-::npm list --depth=0
-::--trace-warnings - отработал неделю без 2-й ошибки
-node --trace-warnings --expose-gc essence_bot.js
+For /F %%v in ('node --version') do set node_ver=%%v
+
+If DEFINED node_ver (
+    If Not EXIST "node_modules" npm install
+    ::npm list --depth=0
+    TITLE Discord Bot - %~0
+    ::--trace-warnings - РѕС‚СЂР°Р±РѕС‚Р°Р» РЅРµРґРµР»СЋ Р±РµР· 2-Р№ РѕС€РёР±РєРё
+    node --trace-warnings --expose-gc essence_bot.js
+)
 
 PAUSE
